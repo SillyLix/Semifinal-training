46e598b8c60a937ae638be4e88538d03650d7450
SillyLix <147418042+SillyLix@users.noreply.github.com>
2025-01-29 23:00:19 +0200

    better movement

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -1,130 +1,135 @@
+using System;
 using UnityEngine;
 
-// This script is for 2D movement with drag and without physics
-// You can use this script for 2D platformer games and 2D top down games
-// made by sillylix 2025 - https://www.sillylix.com/
-
-public class PlayerMovement2D : MonoBehaviour
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
 {
-    // --- Player Movement Settings ---
     [Header("Movement Settings")]
-    [Space(10)]
-    [SerializeField] public bool horizontalMovementNeeded = true;
-    [SerializeField] public bool verticalMovementNeeded = false;
-    [SerializeField] private float playerSpeed = 5f;
+    public bool isTopDown = true;
+    public bool useLinearMovement = true;
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float jumpForce = 5f;
+    public bool waiting;
+    public bool canJump = true;
+    public bool isRunning;
 
-    [Header("Rigidbody Movement Settings")]
-    [Space(10)]
-    [Tooltip("Enable this if you want to use Rigidbody for movement.")]
-    [SerializeField] private bool useRigidbodyForMovement = true;
-
-    // --- Jump Settings ---
-    [Header("Jump Settings")]
-    [Space(10)]
-    [Tooltip("Enable this if you want the player to jump.")]
-    [SerializeField] private bool jumpNeeded = false;
-    [SerializeField] private float jumpForce = 15f;
-    [SerializeField] private float jumpDetectorLength = 1.5f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private bool drawJumpDetector = false;
-
-    // --- Private Variables ---
-    private float horizontalInput;
-    private float verticalInput;
-    private bool isGrounded;
-    private Rigidbody2D rb2d;
-
-
-    void Start()
-    {
-        // get the rigidbody2d component
-        rb2d = gameObject.GetComponent<Rigidbody2D>();
-        rb2d.gravityScale = 0;
-
-    }
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 movement;
+    public bool isGrounded;
+    private bool facingRight = true;
+    private float stillTime;
 
     void Update()
     {
-        // check if we need to use physics for movement or not
-        if (useRigidbodyForMovement)
+        HandleInput();
+        UpdateAnimator();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void HandleInput()
+    {
+        if (rb.linearVelocity.magnitude < 0.05f && isGrounded)
         {
-            MovementWithPhysics();
+            if (!waiting)
+            {
+                stillTime = Time.time;
+                waiting = true;
+            }
         }
         else
         {
-            MovementWithoutPhysics();
+            waiting = false;
         }
 
-        // check if we need to jump or not
-        if (jumpNeeded)
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = isTopDown ? Input.GetAxisRaw("Vertical") : 0;
+        movement = new Vector2(moveX, moveY).normalized;
+
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        if (canJump && !isTopDown && Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            // set the gravity on and jump if we are grounded
-            rb2d.gravityScale = 1;
-            if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        }
+
+        if (moveX > 0 && !facingRight)
+            Flip();
+        else if (moveX < 0 && facingRight)
+            Flip();
+
+        if (Input.GetKeyDown(KeyCode.S) && rb.linearVelocity.magnitude < 0.1f)
+        {
+            animator.SetInteger("Random", Convert.ToInt32(UnityEngine.Random.Range(0, 4)));
+            animator.SetBool("IsSitting", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetInteger("Random", 0);
+            animator.SetBool("IsSitting", false);
+        }
+    }
+
+    void Move()
+    {
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        if (useLinearMovement)
+        {
+            rb.linearVelocity = new Vector2(movement.x * currentSpeed, rb.linearVelocity.y);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocity.x, movement.x * currentSpeed, 0.1f), rb.linearVelocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        animator.SetTrigger("Jump");
+        isGrounded = false;
+    }
+
+    void UpdateAnimator()
+    {
+        float boredness = waiting ? Time.time - stillTime : 0;
+        animator.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("Bored", boredness);
+        animator.SetBool("Falling", rb.linearVelocity.y < -0.1f && !isGrounded);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isTopDown)
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
             {
-                rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                if (contact.normal.y > 0.5f)
+                {
+                    isGrounded = true;
+                    return;
+                }
             }
         }
     }
 
-    // check if we are grounded or not
-    private void FixedUpdate()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        // raycast for detecting the ground
-        Ray ray = new Ray(transform.position + Vector3.up * -0.11f, Vector2.down);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, jumpDetectorLength, groundLayer);
-
-        // draw the jump detactor if on unity editor & drawJumpDetactor is true
-#if UNITY_EDITOR
-        if (drawJumpDetector)
-        {
-            Debug.DrawRay(ray.origin, ray.direction * jumpDetectorLength, Color.red);
-        }
-#endif
-        // set verible
-        if (hit.collider != null)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
+        isGrounded = false;
     }
 
-    void MovementWithPhysics()
+    void Flip()
     {
-        // use physics for movement and cheack if we need to move horizontally or vertically
-        if (horizontalMovementNeeded )
-        {
-            horizontalInput = Input.GetAxis("Horizontal");
-            rb2d.linearVelocity = new Vector2(horizontalInput * playerSpeed, rb2d.linearVelocity.y);
-        }
-        if (verticalMovementNeeded)
-        {
-            rb2d.gravityScale = 0;
-
-            verticalInput = Input.GetAxis("Vertical");
-            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, verticalInput * playerSpeed);
-        }
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
-
-    void MovementWithoutPhysics()
-    {
-        // use transform for movement and cheack if we need to move horizontally or vertically
-
-        if (horizontalMovementNeeded)
-        {
-            horizontalInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector2.right * horizontalInput * playerSpeed * Time.deltaTime);
-        }
-        if (verticalMovementNeeded)
-        {
-            rb2d.gravityScale = 0;
-
-            verticalInput = Input.GetAxis("Vertical");
-            transform.Translate(Vector2.up * verticalInput * playerSpeed * Time.deltaTime);
-        }
-    }
-
 }
